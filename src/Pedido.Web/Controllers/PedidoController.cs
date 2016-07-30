69f4c803afd19201abb3beb92251e09583ed3e77
JoãoAmaro <JooAmaro@ULTRA-JOAO>
2016-07-30 19:55:12 -0300

    site

---
+++ src/Pedido.Web/Controllers/PedidoController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace Pedido.Web.Controllers
+{
+    public class PedidoController : Controller
+    {
+
+        public async Task<IActionResult> RegistrarPedido()
+        {
+
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("http://localhost:5000/");
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                var pedido = new { IdPessoa = 10 };
+
+                var response = await client.PostAsJsonAsync("/api/pedido", pedido);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    return Ok();
+                }
+
+            }
+
+            return Ok();
+        }
+
+    }
+}
+
